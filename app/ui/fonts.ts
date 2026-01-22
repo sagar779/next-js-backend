d8ed6d2d02b9f3e58e7a99ddd6df199222844883
Sagar779 <sagarsinghroithan40@gmail.com>
2026-01-22 13:42:20 +0530

    innitial project commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import '@/app/ui/global.css';
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });

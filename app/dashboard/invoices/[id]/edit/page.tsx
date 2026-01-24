9dc7cdc036df72384f5e2736acbf2c3a0b59cac6
Sagar779 <sagarsinghroithan40@gmail.com>
2026-01-24 12:59:18 +0530

    added error handeling in the invoice page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 import From from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
-import { customers } from '@/app/lib/placeholder-data';
+import {notFound} from 'next/navigation';
 
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
@@ -11,6 +11,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
         fetchInvoiceById(id),
         fetchCustomers()
     ]);
+
+    if (!invoice) {
+        return notFound();
+    }
     return (
         <main>
             <Breadcrumbs
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -28,11 +28,17 @@ export async function createInvoice(formData: FormData) {
   const amountNumber = amount*100;
   const date = new Date().toISOString().split('T')[0];
 
+  try{
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountNumber}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    console.error(error);
+    return {
+      message: 'Database Error creating invoice',
+    }
+  };
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -44,16 +50,25 @@ export async function updateInvoice(id: string, formData: FormData) {
     status: formData.get('status'),
   });
   const amountNumber = amount*100;
+
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountNumber}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: 'Database Error updating invoice',
+    }
+  };
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
   await sql`
     DELETE FROM invoices
     WHERE id = ${id}

b2d5d5fb0510e9c8736bd7413359b2ad46deaa95
Sagar779 <sagarsinghroithan40@gmail.com>
2026-01-28 19:25:02 +0530

    aded the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Next.js Dashboard',
+  description: 'A dashboard application built with App router.',
+  metadataBase: new URL('https://example.com'),
+};
 
 export default function RootLayout({
   children,
